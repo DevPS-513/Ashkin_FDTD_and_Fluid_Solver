86c4ed5b826d22214c89f7ba3e72ddc800b1da3b
maxbw117 <max.bethune.waddell@gmail.com>
2018-04-15 21:19:44 -0700

    Add files via upload

---
+++ Figure_Create.m
@@ -0,0 +1,203 @@
+clear all
+close all
+clc
+
+addpath output_data
+addpath EM_functions
+addpath functions
+addpath material_data
+
+
+s_d=get(0,'ScreenSize');  % Screen [0 0 width height]
+sw=s_d(3);                % Screen width
+sh=s_d(4);                % Screen height
+
+models=cellstr(['AB ';]);
+% models=cellstr(['MN'])
+s_cases=cellstr(['up']);
+mkdir('./output_data/')
+mkdir('./Figures')
+
+
+
+    
+    for case_j=1:length(s_cases)
+        
+    
+    source_direction=char(s_cases(case_j));
+       AB=load(strcat(pwd,'./output_data/','AB','_',source_direction));
+        MN=load(strcat(pwd,'./output_data/','MN','_',source_direction));
+  EL=load(strcat(pwd,'./output_data/','EL','_',source_direction));
+    AMP=load(strcat(pwd,'./output_data/','AMP','_',source_direction));
+dx=AB.data.parameters(1);       
+dy=AB.data.parameters(2);   
+Nx=AB.data.parameters(3);       
+Ny=AB.data.parameters(4); 
+dt=AB.data.parameters(5); 
+Nt=AB.data.parameters(6); 
+%fig_count=data.parameters(7); 
+fig_count=1;
+
+for iq=400:Nt
+ 
+   
+    xf_AB=AB.data.(strcat('AB','_xf_',source_direction,'_',num2str(iq)));
+    yf_AB=AB.data.(strcat('AB','_yf_',source_direction,'_',num2str(iq)));
+      dx=AB.data.parameters(1); 
+      dy=dx;
+     eta_AB=AB.data.(strcat('AB','_eta_EM_',source_direction,'_',num2str(iq)));
+     Hz_AB=AB.data.(strcat('AB','_Hz_EM_',source_direction,'_',num2str(iq)));
+r_AB=AB.data.(strcat('AB','_r_',source_direction,'_',num2str(iq)));
+
+fx_EM_AB=AB.data.(strcat('AB','_fx_EM_',source_direction,'_',num2str(iq)));
+ fy_EM_AB=AB.data.(strcat('AB','_fy_EM_',source_direction,'_',num2str(iq)));   
+ 
+ 
+    xf_MN=MN.data.(strcat('MN','_xf_',source_direction,'_',num2str(iq)));
+    yf_MN=MN.data.(strcat('MN','_yf_',source_direction,'_',num2str(iq)));
+    
+    xf_EL=EL.data.(strcat('EL','_xf_',source_direction,'_',num2str(iq)));
+    yf_EL=EL.data.(strcat('EL','_yf_',source_direction,'_',num2str(iq)));
+    
+    xf_AMP=AMP.data.(strcat('AMP','_xf_',source_direction,'_',num2str(iq)));
+    yf_AMP=AMP.data.(strcat('AMP','_yf_',source_direction,'_',num2str(iq)));
+  
+    
+%     fx=data.(strcat(model,'_fx_',source_direction,'_',num2str(iq*dt*1E6,'%u')));
+%     fy=data.(strcat(model,'_fy_',source_direction,'_',num2str(iq*dt*1E6,'%u')));
+%     fx_EM=data.(strcat(model,'_fx_EM_',source_direction,'_',num2str(iq*dt*1E6,'%u')));
+%     fy_EM=data.(strcat(model,'_fy_EM_',source_direction,'_',num2str(iq*dt*1E6,'%u')));
+
+%% FIGURE INITIALIZE
+if iq==400
+    f_1=figure(1);
+    h_11=fill(xf_AB(3:end-2),yf_AB(3:end-2),[136/255 216/255 247/255],'linewidth',2);
+    hold on
+    h_12=plot(xf_MN(3:end-2),yf_MN(3:end-2),'--r','linewidth',3);
+    xlim([-dx/2 Nx*dx])
+    ylim([-dy/2 Ny*dy])
+%     legend('AB','MN')
+    set(f_1,'position',[0 sh/2-35 sw/3 sh/2])
+    axis off
+    axis equal
+ 
+    
+    
+    f_2=figure(2);
+    h_21=plot(xf_AB(2:end-1),yf_AB(2:end-1),'linewidth',2.5);
+    hold on
+    h_22=plot(xf_EL(2:end-1),yf_EL(2:end-1),'--black','linewidth',2);
+    xlim([-dx/2 Nx*dx])
+    ylim([-dy/2 Ny*dy])
+    legend('AB','EL')
+    set(f_2,'position',[sw/3 sh/2-35 sw/3 sh/2])
+    
+    f_3=figure(3);
+    h_31=plot(xf_AB(2:end-1),yf_AB(2:end-1),'linewidth',2.5);
+    hold on
+    h_32=plot(xf_AMP(2:end-1),yf_AMP(2:end-1),'--y','linewidth',2);
+    xlim([-dx/2 Nx*dx])
+    ylim([-dy/2 Ny*dy])
+    legend('AB','AMP')
+    set(f_3,'position',[2*sw/3 sh/2-35 sw/3 sh/2])
+    
+else
+       if iq==1000
+  
+        [Nx_EM,Ny_EM]=size(Hz_AB);
+        dx_em=1./(Nx_EM-1);
+        dy_em=1./(Ny_EM-1);
+        x_em=[0:1:Nx_EM-1]*dx_em;
+        y_em=[0:1:Ny_EM-1]*dy_em;
+  
+        [Nx Ny]=size(r_AB);
+        Nx=Nx-1;
+        Ny=Ny-1;
+        
+
+        max_f=max(max(sqrt(fx_EM_AB(1:Nx-1,1:Ny-1).^2+fy_EM_AB(1:Nx-1,1:Ny-1).^2)));
+        fx_EM=fx_EM_AB./max_f;
+        fy_EM=fy_EM_AB./max_f;
+        
+        fx_EM=fx_EM*20/Nx;
+        fy_EM=fy_EM*20/Ny;
+
+        fx_EM=fx_EM(:,1:end-1);
+        fy_EM=fy_EM(1:end-1,:);
+        
+        
+        % being all values to middle
+        
+        
+
+        
+        
+        x=[0:1:Nx-1]./(Nx-1);
+        y=[0:1:Ny-1]./(Ny-1);
+        
+        for i=1:2:Nx-1
+            for j=1:2:Ny-1
+                
+                
+                
+                f_mag=sqrt(fx_EM(i,j)^2+fy_EM(i,j)^2)*(Ny/20);
+                if(f_mag>.35)
+                    
+                    fx_EM(i,j)=.5*(fx_EM(i,j)+fx_EM(i,j+1));
+                    fy_EM(i,j)=.5*(fy_EM(i,j)+fy_EM(i+1,j));
+                    
+                    figure(10)
+                    hold on
+                    h=quiver(x(i),y(j),fx_EM(i,j),fy_EM(i,j));
+                    set(h,'color','r');
+                                     hold on                   
+                    
+                    
+                end
+                
+                
+                
+            end
+            
+        end
+        
+     figure(1)
+        hold off
+        
+  %     saveas(f_1,'./Figures/Ashkin_geom','png'); 
+    end
+    
+    
+    set(h_11,'XData',xf_AB(3:end-3),'YData',yf_AB(3:end-3))
+        set(h_12,'XData',xf_MN(2:end-1),'YData',yf_MN(2:end-1))
+    set(h_21,'XData',xf_AB(2:end-1),'YData',yf_AB(2:end-1))
+ set(h_22,'XData',xf_EL(2:end-1),'YData',yf_EL(2:end-1))
+ 
+     set(h_31,'XData',xf_AB(2:end-1),'YData',yf_AB(2:end-1))
+ set(h_32,'XData',xf_AMP(2:end-1),'YData',yf_AMP(2:end-1))
+ 
+drawnow
+ 
+end
+if iq>250
+%     pause(.1);
+end
+    iq
+end
+    
+    
+
+%  clf
+
+end
+
+
+       
+        
+        
+        
+        
+        
+        
+        
+
--- MAIN.m
+++ MAIN.m
@@ -2,16 +2,74 @@ clear all
 close all
 clc
 
+
+% This code solves navier stokes equation for two fluids seperated by a
+%  defined "front" it uses the front tracking tutorial found at https://www3.nd.edu/~gtryggva/MultiphaseDNS/DNS-Solver.pdf
+%  and within the "Reference Documents" folder of this code.
+
+
+% An example of how to plot the ouput data will be uploaded soon, in the
+%  meantime you can load the output after the simulation via commands such
+%  as "AB_down=load('./output_data/AB_down.mat')"
+%  for the AB form and a beam pointed downward.
+%  retreive simulation data as 
+%  [dx dy Nx Ny dt Nt fig_count rho1 rho2 er_low er_high]=AB_down.parameters
+%  then plot lets say the fy distribution at n=100 via
+%  surf(AB_down.AB_fy_down_100)
+
+% As well, after "beam_start", an FDTD code is run that solves for the
+%  electromagnetic fields where the "front" between the two fluids is treated as
+%  the interface between two media with two different refractive indicies.
+%  the FDTD code has absorbing boundary conditions and only considers one
+%  interface of the droplet. The force density is recalculated after
+%  "beam_recalculate" time has passed
+
+
+
+% Important points
+
+% First and foremost, this is a continuum fluid model under the influence of
+% various models for EM force density, but has not at this time been
+% verified to represent the actual fluid velocity values that one would
+% expect, and typically the simulation is unstable for anything not on the
+% scale of meters, a CFD student may be able to read tryggvasons
+% tutorial and then figure out why this is, but i'm not sure
+
+% Make sure "beam_recalculate_time" is not too long. It should be 1 fluid
+%   dt, but the simulation will take a drastically long time and you will
+%   notice no appreciable change in the force density until the EM force
+%   actually moves the front to a different shape, which may take several
+%   miliseconds
+% If gravity is turned on, the fluid will take the majority of the
+%   simulation time just settling into a stationary state. It also needs this
+%   time so that a flat surface is created. 
+% The default setting is a [Lx X Ly] = [1m X 1m] liquid enviroment,
+% the electromagnetic force is then scaled up to this size and normalized
+% to a its peak value, which can then be multiplied
+
+% Important parameters
+
+% Nx and Ny are the x and y fluid resolution
+
+% ratio_of_EM_force_to_gravity: The EM force is normalized, and 
+% this constant determines how strong it should be considered in the fluid
+% simulation relative to gravity. This is to make the deformation occur on
+% meaningful time scales that the simulation can accomplish, and to make
+% sure the force is not too small or too large.
+
 addpath output_data
 addpath EM_functions
 addpath functions
 addpath material_data
 
 
-%models=cellstr(['AB ';'MN ';'EL ';'AMP']);
-  models=cellstr(['AB ';'Chu']);
+% models=cellstr(['AB ';'MN ';'EL ';'AMP';'Chu']);
+
+  
+ models=cellstr(['MN ';'EL ';'AMP';'Chu']);
 % models=cellstr(['MN'])
 s_cases=cellstr(['down';'up  ']);
+
 mkdir('./output_data/')
 
 
@@ -25,57 +83,71 @@ for model_j=1:length(models)
    
     model=char(models(model_j));
     s_case=char(s_cases(case_j));
-    %% SI units
-meters=1;
-nm=meters*1e-9;
-femptoseconds=1e-15;
-mu_o=4*pi*10^-7;
-c=299792458;
-eps_o=(1/(c*c*mu_o));
-eta_o=sqrt(mu_o/eps_o);
-Kg=1;    
-plot_on=1;
-plot_on_EM=1;
-save_mode=1;
+    
+ %% SI units
+
+    meters=1;
+    nm=meters*1e-9;
+    seconds=1;
+    femptoseconds=1e-15;
+    mu_o=4*pi*10^-7;
+    c=299792458;
+    eps_o=(1/(c*c*mu_o));
+    eta_o=sqrt(mu_o/eps_o);
+    Kg=1;    
+                   % The simulation is complete
+    
 %% FIGURE INITIALIZATION
 
-fig_count=1;
-n_count=0;
-s_d=get(0,'ScreenSize');  % Screen [0 0 width height]
-sw=s_d(3);                % Screen width
-sh=s_d(4);                % Screen height
+    
+ % Turn Figure plotting on or off    
+    plot_on=0;                      % This will display Density over time
+    plot_on_EM=0;                   % This will display the EM field
+    save_mode=1;                    % This will save all workspace variables after
+                 
+
+    fig_count=1;
+    n_count=0;
+    s_d=get(0,'ScreenSize');  % Screen [0 0 width height] used to make figure sizes relative to the 
+                              % screen size
+    sw=s_d(3);                % Screen width
+    sh=s_d(4);                % Screen height
 
 % Figure Positions
-p1 = [1 sh/2 sw/3 sh/2];
-p2 = [sw/3 sh/2 sw/3 sh/2];
-p3 = [1 35 sw/3 sh/2];
-p4 = [sw/3 35 sw/3 sh/2];
-p5 = [sw/3+p3(3) sh/2 sw/3 sh/2];
+% The figures are placed in a 3x3 array viewed on the screen
+
+    p1 = [1 sh/2 sw/3 sh/2];
+    p2 = [sw/3 sh/2 sw/3 sh/2];
+    p3 = [1 35 sw/3 sh/2];
+    p4 = [sw/3 35 sw/3 sh/2];
+    p5 = [sw/3+p3(3) sh/2 sw/3 sh/2];
+    
 if plot_on==1
-f_1=figure(1);
-set(f_1,'name','Front and <F>')
-set(f_1,'DefaulttextFontSize',14)
-set(f_1,'OuterPosition',p1)
+    
+    f_1=figure(1);
+    set(f_1,'name','Front and <F>')
+    set(f_1,'DefaulttextFontSize',14)
+    set(f_1,'OuterPosition',p1)
 
-f_2=figure(2);
-set(f_2,'name','index in FDTD')
-set(f_2,'DefaulttextFontSize',14)
-set(f_2,'OuterPosition',p2)
+    f_2=figure(2);
+    set(f_2,'name','index in FDTD')
+    set(f_2,'DefaulttextFontSize',14)
+    set(f_2,'OuterPosition',p2)
 
-f_3=figure(3);
-set(f_3,'name','Density')
-set(f_3,'DefaulttextFontSize',14)
-set(f_3,'OuterPosition',p3)
+    f_3=figure(3);
+    set(f_3,'name','Density')
+    set(f_3,'DefaulttextFontSize',14)
+    set(f_3,'OuterPosition',p3)
 
-f_4=figure(4);
-set(f_4,'name','Hz_field')
-set(f_4,'DefaulttextFontSize',14)
-set(f_4,'OuterPosition',p4)
+    f_4=figure(4);
+    set(f_4,'name','Hz_field')
+    set(f_4,'DefaulttextFontSize',14)
+    set(f_4,'OuterPosition',p4)
 
-f_5=figure(5);
-set(f_5,'name','\eta in FDTD')
-set(f_5,'DefaulttextFontSize',14)
-set(f_5,'OuterPosition',p5)
+    f_5=figure(5);
+    set(f_5,'name','\eta in FDTD')
+    set(f_5,'DefaulttextFontSize',14)
+    set(f_5,'OuterPosition',p5)
 
 
 end
@@ -84,246 +156,260 @@ end
 
 %% CFD Parameters
 
-Lx=1;
-Ly=1;
-Nx=72;
-Ny=72;
-source_direction=s_case;
+    Lx=1;
+    Ly=1;
+    Nx=72;
+    Ny=72;
+    source_direction=s_case;
 
-time_steps=9680;
-% Grid step sizes
-dx=Lx/Nx;
-dy=Ly/Ny;
+    time_steps=9680;
+    dt=.00125;                  % CFD time step
+    
+    beam_start_time=8*seconds;    
+    beam_start=round(beam_start_time/dt);         % number of time steps before beam engages
+   
+    beam_recalculate_time=0.2*seconds;          % Wait this amount of time before recalculateing the force distribution
+    beam_recalculate=round(beam_recalculate_time/dt);
+   
+    beam_refresh_counter=0;                         % Counter that rolls over every
+    
+    
+    % Grid step sizes
+    dx=Lx/Nx;
+    dy=Ly/Ny;
 
 % Iteration parameters 
-iterations=50;
-tol=1;
-beta=1.2;
+
+    iterations=50;
+    tol=1;
+    beta=1.2;
+    
 
 
 %% CFD DATA, DENSITY AND SURFACE TENSION
-rho1=1; rho2=1.2;
-min_rho=min([ rho1 rho2]);
-rho_diff=abs(rho1-rho2)/2;
 
-surf_tension=.2;
-mu1=.01;
-mu2=2*mu1;
-max_mu=max([mu1 mu2]);
-% time step criterion
-%dt=(1/30)*(1/4)*min_rho*(min([ dx dy]))^2/(max_mu);
-dt=.00125;
+    rho1=1;                     % Density of fluid that represents air
+    rho2=1.2;                   % Density of fluid that represents water 
+        
+    min_rho=min([ rho1 rho2]);
+    rho_diff=abs(rho1-rho2)/2;
 
-t=[0:1:time_steps]*dt;
-Nt=time_steps;
+    surf_tension=0.3;            % Surface Tension Coefficient
+    mu1=.01;
+    mu2=2*mu1;
+    max_mu=max([mu1 mu2]);
 
 
-% Initialize velocity and pressure, and density
-F1x_avg=zeros(1,Nt);
-F1y_avg=zeros(1,Nt);
-F2x_avg=zeros(1,Nt);
-F2y_avg=zeros(1,Nt);
-u=zeros(Nx+1,Ny+2);         %   u [m/s]
-v=zeros(Nx+2,Ny+1);         %   v [m/s]
-p=zeros(Nx+2,Ny+2);         %   p [pa]
-u_star=zeros(Nx+1,Ny+2);    %   ut[m/s]
-v_star=zeros(Nx+2,Ny+1);    %   vt[m/s]
+    t=[0:1:time_steps]*dt;
+    Nt=time_steps;
 
-grad_rx=zeros(Nx+2,Ny+2);   % density x-gradient
-grad_ry=zeros(Nx+2,Ny+2);   % density y-gradient 
+
+%% Initialize output matricies
+
+
+    u=zeros(Nx+1,Ny+2);         %   u [m/s]
+    v=zeros(Nx+2,Ny+1);         %   v [m/s]
+    p=zeros(Nx+2,Ny+2);         %   p [pa]
+    u_star=zeros(Nx+1,Ny+2);    %   ut[m/s]
+    v_star=zeros(Nx+2,Ny+1);    %   vt[m/s]
+
+    grad_rx=zeros(Nx+2,Ny+2);   % density x-gradient
+    grad_ry=zeros(Nx+2,Ny+2);   % density y-gradient 
 
 % Initialize Boundary conditions
-u_wall_top=0;
-u_wall_bottom=0;
-v_wall_left=0;
-v_wall_right=0;
+
+    u_wall_top=0;
+    u_wall_bottom=0;
+    v_wall_left=0;
+    v_wall_right=0;
+    
 % Initialize Gravity
-gx=zeros(size(u));
-gy=-1*100*ones(size(v));
+
+    gx=zeros(size(u));
+    gy=-1*100*ones(size(v));
 
 % x and y axis
-x=zeros(1,Nx+2);
-y=zeros(1,Ny+2);
+    x=zeros(1,Nx+2);
+    y=zeros(1,Ny+2);
 
-for i=1:Nx+2;   
- x(i)=dx*(i-1.5);
+for (i=1:Nx+2)  
+    x(i)=dx*(i-1.5);
 end
 
-for j=1:Ny+2;     
-y(j)=dy*(j-1.5);
+for (j=1:Ny+2)     
+    y(j)=dy*(j-1.5);
 end
 
-%% FDTD data
+%% FDTD and index of refraction data data
 
-er_high=1.33^2;   % permitivty where HIGH DENSITY is
-er_low=1.00^2;       % permitivity where LOW DENSITY is
+    er_high=1.33^2;         % permitivty where HIGH DENSITY is i.e water
+    er_low=1.00^2;          % permitivity where LOW DENSITY is i.e air
 
-a=4000;          % amplitude of the electric field
-LAMBDA=500*nm;  % wavelength of the source
-dx_EM=32*nm;    
-dy_EM=dx;
-beam_width=2*LAMBDA;
-x_size=3*beam_width;
-y_size=(Ly/Lx)*x_size;
-sim_cycles=1.0*y_size/(LAMBDA/max([er_low er_high]));
-beam_start=300;
+    a=4000;                 % amplitude of the electric field
+    LAMBDA=500*nm;          % wavelength of the source
+    dx_EM=32*nm;    
+    dy_EM=dx;
+    beam_width=2*LAMBDA;
+    x_size=3*beam_width;
+    y_size=(Ly/Lx)*x_size;
+    sim_cycles=1.0*y_size/(LAMBDA/max([er_low er_high]));
+    
+                            % this should only be after the fluid conforms
+                            % to the container (i.e settles down)
 
-%% 2.0 Define Density and permitivity
+    n_low=sqrt(er_high);
+    n_high=sqrt(er_low);
+    lam_low=LAMBDA/n_low;
+    lam_high=LAMBDA/n_high;
+    intercept=Ly/(1+lam_high/lam_low);  % Define where boundary between two liquids
+                                        % should be
 
-n_low=sqrt(er_high);
-n_high=sqrt(er_low);
-lam_low=LAMBDA/n_low;
-lam_high=LAMBDA/n_high;
-intercept=Ly/(1+lam_high/lam_low);
+                                        ratio_of_EM_force_to_gravity=20;
+% Initialize center of water source droplet droplet
+    x_offset=7*dx;
+    nx1=round(x_offset/dx)+1;
+    nx2=round((x(end)-x_offset)/dx)+1;
+    ny1=round(x_offset/dy)+1;
+    ny2=round(intercept/dy)+1;
 
-% Initialize center of droplet
-x_offset=7*dx;
-nx1=round(x_offset/dx)+1;
-nx2=round((x(end)-x_offset)/dx)+1;
-ny1=round(x_offset/dy)+1;
-ny2=round(intercept/dy)+1;
+    xc=mean(x);
+    yc=mean(y);
+    rad=(x(end)-x(1))/5;
+    r=rho1.*ones(Nx+2,Ny+2);
+    
+    % Find equivalent circle
+    % pir^2=L*W,r=sqrt(L*w/pi)
+        
 
-xc=mean(x);
-yc=mean(y);
-rad=(x(end)-x(1))/5;
-r=rho1.*ones(Nx+2,Ny+2);
-r(nx1:nx2,ny1:ny2)=rho2;
-% 
-% for i=1:Nx
-%     for j=1:Ny
-%         
-%         c1=(x(i)-xc)^2+(y(j)-yc)^2;
-%         
-%         if (c1<=rad^2)
-%            r(i,j)=rho2; 
-%         end
-%         
-%     end
-% end
-% 
+        x_ellipse=round((round(Nx/2)-1));
+        y_ellipse=round((ny2-ny1)/2.5);
+        
+        mid_y=round(2+y_ellipse);
+        mid_x=round((Nx+2)/2);
 
+        n_ellipse=6;
+        
+    for i=1:Nx+2
+        for j=1:Ny+2
+            
+            if ( ( abs(((i-mid_x)/x_ellipse)^n_ellipse)+abs(((j-mid_y)/y_ellipse)^n_ellipse) )<1)
+            r(i,j)=rho2;
+            end
+        end
+    end
+    
+   % r(nx1:nx2,ny1:ny2)=rho2;
 
-[Nf,xf,yf]=create_front_v2(r,x,y,dx,dy);
-
-
-mu=mu1.*(r==rho1)+mu2.*(r==rho2);
-
-%%  Initialize Front
-
-
+    [Nf,xf,yf]=create_front_v2(r,x,y,dx,dy);
+    mu=mu1.*(r==rho1)+mu2.*(r==rho2);
 
 %Create from density contour
 % %re-size front based on current grid
-  [xf,yf,Nf]=resize_front(xf,yf,dx,dy,Nf);
+
+    [xf,yf,Nf]=resize_front(xf,yf,dx,dy,Nf);
+    
 % front velocities
+
     uf=zeros(1,Nf+2);
     vf=zeros(1,Nf+2);
-% front tangent vectors     
+    
+% front tangent vectors    
+
     tx=zeros(1,Nf+2);
     ty=zeros(1,Nf+2);
+    
 % surface force
+
     fx=zeros(size(u));
     fy=zeros(size(v));     
  
     %% FIGURE CREATE
 if plot_on==1
-% f_2=figure(1);
-% h_11=plot(xf(1:Nf)./(x(end)-x(1)),yf(1:Nf)./(x(end)-x(1)));
-% xlabel('x-axis[m]')
-% ylabel('y-axis')
-% xlim([x(1)-dx x(end)+dx])
-% ylim([y(1)-dy y(end)+dy])
-% line([ x(1) x(1)],[y(1) y(end)],'color','black')
-% line([ x(end) x(end)],[y(1) y(end)],'color','black')
-% line([ x(1) x(end)],[y(1) y(1)],'color','black')
-% line([ x(1) x(end)],[y(end) y(end)],'color','black')
-% axis equal
-% title(strcat(model,'_{',source_direction,'}'))
-figure(3)
-h_31=surf(x,y,r');
-xlim([-.1 1.1])
-ylim([-.1 1.1])
-caxis([ .9 1.3])
-shading flat
-view([ 0 90])
-hold on
-h_32=plot3(xf(3:end-1),yf(3:end-1),10.*ones(size(xf(3:end-1))),'color','black','linewidth',1.9);
-xlabel('x')
-ylabel('y')
-axis equal
-colorbar east
-title(strcat(model,'_{',source_direction,'}'))
+    % f_2=figure(1);
+    % h_11=plot(xf(1:Nf)./(x(end)-x(1)),yf(1:Nf)./(x(end)-x(1)));
+    % xlabel('x-axis[m]')
+    % ylabel('y-axis')
+    % xlim([x(1)-dx x(end)+dx])
+    % ylim([y(1)-dy y(end)+dy])
+    % line([ x(1) x(1)],[y(1) y(end)],'color','black')
+    % line([ x(end) x(end)],[y(1) y(end)],'color','black')
+    % line([ x(1) x(end)],[y(1) y(1)],'color','black')
+    % line([ x(1) x(end)],[y(end) y(end)],'color','black')
+    % axis equal
+    % title(strcat(model,'_{',source_direction,'}'))
+    
+    % Figure of fluid density
+    figure(3)
+    h_31=surf(x,y,r');
+    xlim([-.1 1.1])
+    ylim([-.1 1.1])
+    caxis([ .9 1.3])
+    shading flat
+    view([ 0 90])
+    hold on
+    h_32=plot3(xf(3:Nf-4),yf(3:Nf-4),10.*ones(size(xf(3:Nf-4))),'color','black','linewidth',1.9);
+    Nf
+    length(xf)
+    xlabel('x')
+    ylabel('y')
+    axis equal
+    title(strcat(model,'_{',source_direction,'}'))
 end
 
-% store density,impedance,Hz field, fx and fy, for latert
-fig_counter=0;
-N_fig=round(time_steps/fig_count);
-r_fig=zeros(Nx+2,Ny+2,N_fig);
-u=zeros(Nx+1,Ny+2);     %   u [m/s]
-v=zeros(Nx+2,Ny+1);     %   v [m/s]
-fx_fig=zeros(Nx+1,Ny+2,N_fig);
-fy_fig=zeros(Nx+2,Ny+1,N_fig);
+% Prepare figure variables 
 
+    fig_counter=0;                      % Counter to set plot refresh
+    N_fig=round(time_steps/fig_count);  % number of steps until a figure refresh
 
-
-
+%     fx_fig=zeros(Nx+1,Ny+2,N_fig);
+%     fy_fig=zeros(Nx+2,Ny+1,N_fig);
+    
 %% 4.0 Time Loop
 
-%load a pre-made front and density
+    fx_EM=0;
+    fy_EM=0;
+    eta_EM=0;
+    Hz_EM=0;
+    disp_max=0;   % Amount of displaecment in fluid domain to wait until
+    xf_old=xf;
+    yf_old=yf;
 
-% init=load('init');
-% Nf=init.Nf;
-% xf=init.xf;
-% yf=init.yf;
-% r=init.r;
-
-fx_EM=0;
-fy_EM=0;
-eta_EM=0;
-Hz_EM=0;
-disp_max=0;
-xf_old=xf;
-yf_old=yf;
-
+    [Nx_fx, Ny_fx]=size(u');
+    [Nx_fy, Ny_fy]=size(v');
 
 for n=1:time_steps
     
-% store old front
     
-  
+
+% Erase Previous force values  
     fx=0.*fx;    
     fy=0.*fy;
  
 
+% Start radiation beam if "beam_start" number of timsteps have passed
 if n>beam_start
  
-       %% convert front to a percentage of the grid
     xf_norm=(xf(1:Nf))./(x(end)-x(1));
     yf_norm=(yf(1:Nf))./(y(end)-y(1));
-    c_e=0;
-    
-    % find front location at x=.2, x=.8
-    
-
-    
+    c_e=0;  
+  
     for qf=2:Nf-1
     
-         x1_em=.1;
+    x1_em=.1;
     x2_em=.9;
     c1=(xf_norm(qf)>x1_em);
     c2=(xf_norm(qf)<x2_em);
     c3=(yf_norm(qf)>(.3*(ny2-ny1))/Ny);
     
     
-    
     if(c1&&c2&&c3)
-       c_e=c_e+1;
+        
+        c_e=c_e+1;
         xf_EM(c_e)=xf_norm(qf);
         yf_EM(c_e)=yf_norm(qf);   
         
         c_x1=((abs(xf_norm(qf)-x1_em))<(2/Nx));
-        c_x2=((abs(xf_norm(qf)-x2_em))<(2/Nx));
-        
+        c_x2=((abs(xf_norm(qf)-x2_em))<(2/Nx));        
         
         if c_x1
           y1_em=yf_norm(qf);  
@@ -345,56 +431,43 @@ if n>beam_start
     yl_tack=y1_em.*ones(size(xl_tack));
     yr_tack=y2_em.*ones(size(xr_tack));
 
-  xf_EM=[xl_tack,xf_EM,xr_tack];
-  
-  
-  yf_EM=[yl_tack,yf_EM,yr_tack];
-  
-  
-
-    
-%     for iu_f=1:length(xf_EM)
-%           figure(10)
-%           plot(xf_EM(iu_f),yf_EM(iu_f))
-%           pause(.1)
-%           
-%         
-%     end
-%   
+    xf_EM=[xl_tack,xf_EM,xr_tack];  
+    yf_EM=[yl_tack,yf_EM,yr_tack];
     
     
-if (disp_max>(sqrt(dx^2+dy^2)/2)) 
+    if (beam_refresh_counter>=beam_recalculate) 
 
-   [fx_EM, fy_EM,eta_EM,Hz_EM,Sy_avg,fx_avg,fy_avg]=casner_EM_code_best(x,y,xf_EM,yf_EM,er_low,er_high,LAMBDA,dx_EM,beam_width,x_size,y_size,a,sim_cycles,model,plot_on_EM,source_direction);
+        
+        
+       [fx_EM, fy_EM,eta_EM,Hz_EM,Sy_avg,fx_avg,fy_avg]=FDTD(Nx_fx,Ny_fx,Nx_fy,Ny_fy,x,y,xf_EM,yf_EM,er_low,er_high,LAMBDA,dx_EM,beam_width,x_size,y_size,a,sim_cycles,model,plot_on_EM,source_direction);
+    beam_refresh_counter=0;
+    
+    % fy_EM and fx_EM are normalized to the maximum force value that
+    % exists.
+    % Scale force (note that gravity is '100') 
+    fx_EM=fx_EM.*ratio_of_EM_force_to_gravity;
+    fy_EM=fy_EM.*ratio_of_EM_force_to_gravity;
+    end
 
-   %fx_EM=0.*fx_EM;
-   %fy_EM=0.*fy_EM;
+end
+
+% Reset disp max after it reaches one dex
+    if (disp_max>(sqrt(dx^2+dy^2)/2)) 
+        disp_max=0;
+    end
+    fx=fx+fx_EM;
+    fy=fy+fy_EM;
+
+    str = sprintf(['t(n) is \t' num2str(round(t(n),3)) '(s)\tand\t' num2str(round(100*n/Nt,2)) '\tperc. done']);
+disp([str])
+
+    beam_refresh_counter=beam_refresh_counter+1;
    
-disp_max/dx
-
-end
-
-end
-
-if (disp_max>(sqrt(dx^2+dy^2)/2)) 
-    disp_max=0;
-end
-fx=fx+fx_EM;
-fy=fy+fy_EM;
-
-n
-%    fx=fx+fx_EM(2:end-1,1:end-1);
-%    fy=fy+fy_EM(1:end-1,2:end-1);
-% end
-% fx(nx1:nx2,ny1:end)=fx_EM;
-% fy(nx1:nx2,ny1:end)=fy_EM(:,1:end-1);
-
-     
  % Refresh Boundary Conditions for wall velocities
- u(:,end)=2*(u_wall_bottom)-u(:,end-1);
- u(:,1)=2*(u_wall_top)-u(:,2); 
- v(1,:)=2*v_wall_left-v(2,:);
- v(end,:)=2*v_wall_right-v(end-1,:);   
+     u(:,end)=2*(u_wall_bottom)-u(:,end-1);
+     u(:,1)=2*(u_wall_top)-u(:,2); 
+     v(1,:)=2*v_wall_left-v(2,:);
+     v(end,:)=2*v_wall_right-v(end-1,:);   
 
 % SURFACE TESNSION
     
@@ -527,6 +600,9 @@ xf(Nf+2)=xf(2);
 yf(Nf+2)=yf(2);
 
 
+
+% Check for maximum displacement the front has made, wait until it reaches
+% one dx or dy before calling FDTD
 disp_max=disp_max+max([disp_x disp_y]);
 
 
@@ -583,7 +659,7 @@ if(n_count==fig_count)
 %    end
  % Figure 3- density profile
  set(h_31,'ZData',r')
- set(h_32,'XData',xf,'YData',yf,'ZData',10.*ones(size(xf)))
+ set(h_32,'XData',xf(2:Nf),'YData',yf(2:Nf),'ZData',10.*ones(size(xf(2:Nf))))
 drawnow
 
     end
@@ -624,13 +700,14 @@ clear xf_EM yf_EM;
     
 end
 
+        if save_mode==1
+            save(strcat(pwd,'./output_data/',model,'_',source_direction),'data');
+            save(strcat(pwd,'./output_data/',model,'_',source_direction,'_workspace'));
+        end
 
     end
-if save_mode==1
-    save(strcat(pwd,'./output_data/',model,'_',source_direction),'data');
-    save(strcat(pwd,'./output_data/',model,'_',source_direction,'_workspace'));
 end
-    end
+    
 
 
 
